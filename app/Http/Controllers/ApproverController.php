32d497a8ee742102b79fa797f24c3e3c2f54b728
Novi Aditya <noviaditya.dev@gmail.com>
2024-07-02 15:25:06 +0700

    create separete request class for store approver

--- app/Http/Controllers/ApproverController.php
+++ app/Http/Controllers/ApproverController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreApproverRequest;
 use App\Repositories\ApproverRepository;
-use Illuminate\Http\Request;
 
 class ApproverController extends Controller
 {
 
-    public function addApprover(Request $request){
+    public function addApprover(StoreApproverRequest $request){
+        $input      = $request->validated();
         $approver   = new ApproverRepository();
-        $result     = $approver->store($request->input());
+        $result     = $approver->store($input);
 
         return response()->json($result['data'], $result['code']);
     }
